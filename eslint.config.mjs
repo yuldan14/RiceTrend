179faef43d78b10e0134ed6f9d919c6c6b59ec75
unknown <yuldann6@gmail.com>
2025-07-02 20:44:22 +0700

    footer

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,9 +8,13 @@ const __dirname = dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
-
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
+  ...compat.config({
+    extends: ['next'],
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+    },
+  }),
+]
 export default eslintConfig;
